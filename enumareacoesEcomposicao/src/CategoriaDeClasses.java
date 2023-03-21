76984bdd4fd23497adeb5fd14e11be5162de5fe3
Diego <diegoalencar.jacober@gmail.ocm>
2023-03-21 20:29:53 -0300

    Tipos de classes e composição

---
+++ enumareacoesEcomposicao/src/CategoriaDeClasses.java
@@ -0,0 +1,21 @@
+public class CategoriaDeClasses {
+    // Em um sistema orientado a objetos, de modo geral "tudo" é objeto
+
+    //Por questões de design tais como organização, flexibildade, reuso, delegaçãi, etec., há várias categorias de classes
+    // Views -> Telas do sistema
+    // Controllers -> Controladores entre tela e backend
+    // Entities -> Entidade de negócio
+    // Services -> Representam serviços
+    // Repositories -> Objetos responsaveis por acessar o banco de dados
+
+    //-----------Composição-----------
+    /*
+    *   É um tipo de associação que permite que um objeto contenha outro
+    *   Relação "tem um" ou "tem vários"
+    * Vantagens:
+    *       Organização: divisão de responsabilidades
+    *       Coesão
+    *       Flexibilidade
+    *       Reuso
+    */
+}
