991450d652ad6741d943fea079026978d151bc83
Diego <diegoalencar.jacober@gmail.ocm>
2023-08-29 20:40:01 -0300

    Recuperando dados

--- projeto01/src/application/Program.java
+++ projeto01/src/application/Program.java
@@ -1,14 +1,37 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
 
-        DB.closeConnection();
+        try {
+            conn = DB.getConnection();
+
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + ", " + rs.getString("name"));
+                // System.out.println(rs.getString(1));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(stmt);
+            DB.closeConnection();
+        }
 
     }
 
