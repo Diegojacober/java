3eee1460239c4986362b8327291dd68f75bc4d87
Diego Alencar Jacober <57761933898>
2023-08-31 16:43:12 -0300

    Seller entity class

--- projeto/src/application/Program.java
+++ projeto/src/application/Program.java
@@ -1,18 +1,17 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
 
-import db.DB;
-import db.DbException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
+        System.out.println(seller);
     }
 
 }
