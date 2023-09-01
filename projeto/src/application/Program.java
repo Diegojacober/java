4c0ca0b0971e3a3c731c7d5315964f5414ffc455
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2023-09-01 09:59:29 -0300

    Seller findByDepartment

--- projeto/src/application/Program.java
+++ projeto/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,6 +14,17 @@ public class Program {
 
         System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+
+
+        System.out.println("==== TEST 2: seller findByDepartment ====");
+        Department dep =  new Department(1, null);
+        List<Seller> sellers = (List<Seller>) sellerDao.findByDepartment(dep);
+
+        for (Seller obj : sellers) {
+            System.out.println(obj);
+        }
+
 
         System.out.println(seller);
     }
