76c8222d1cb1262104b7df4ce2f7a212b1b191fc
Diego Alencar Jacober <57761933898>
2023-09-04 14:18:41 -0300

    Service layer e registro de componentes

--- curso/src/main/java/com/diegojacober/curso/repositories/UserRepository.java
+++ curso/src/main/java/com/diegojacober/curso/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.diegojacober.curso.entities.User;
 
+//@Repository //não é necessário pq ja esta extendendo o Jpa repository que ja está registrado
 public interface UserRepository extends JpaRepository<User, Long>{
     
 }
