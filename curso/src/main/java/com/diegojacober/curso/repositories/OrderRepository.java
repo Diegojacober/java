f058ed4b76113dfdd9d210da77ffe21376e6a18e
Diego Alencar Jacober <57761933898>
2023-09-04 14:58:37 -0300

    Order, Instant, ISO 8601 - part 02

---
+++ curso/src/main/java/com/diegojacober/curso/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.diegojacober.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegojacober.curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+    
+}
