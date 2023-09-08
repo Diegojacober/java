ba4c129b87cf819945c1beab7e83c908118c4df1
Diego <diegoalencar.jacober@gmail.ocm>
2023-09-08 08:35:52 -0300

    Product-OrderItem one-to-many association

--- curso/src/main/java/com/diegojacober/curso/entities/Product.java
+++ curso/src/main/java/com/diegojacober/curso/entities/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,12 +29,13 @@ public class Product implements Serializable {
     private String description;
     private String imgUrl;
 
-    //Não pode se repetir então  melhor um set
-     @ManyToMany
-    @JoinTable(name = "tb_product_category",
-     joinColumns = @JoinColumn(name = "product_id"),
-     inverseJoinColumns = @JoinColumn(name = "category_id"))
+    // Não pode se repetir então melhor um set
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> orders = new HashSet<>();
 
     public Product() {
 
@@ -89,6 +93,17 @@ public class Product implements Serializable {
         return categories;
     }
 
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+
+        for (OrderItem x: orders) {
+            set.add(x.getOrder());
+        }
+
+        return set;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -111,5 +126,4 @@ public class Product implements Serializable {
         return true;
     }
 
-    
 }
