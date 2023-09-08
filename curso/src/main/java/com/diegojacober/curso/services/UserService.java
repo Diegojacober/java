8cf39f3308d1d993b8232aec413b0ef913099851
Diego <diegoalencar.jacober@gmail.ocm>
2023-09-08 09:25:39 -0300

    Exception handling - findById

--- curso/src/main/java/com/diegojacober/curso/services/UserService.java
+++ curso/src/main/java/com/diegojacober/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.diegojacober.curso.entities.User;
 import com.diegojacober.curso.repositories.UserRepository;
+import com.diegojacober.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
     public User findById(long id) {
         Optional<User> obj = repository.findById(id);
         
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
